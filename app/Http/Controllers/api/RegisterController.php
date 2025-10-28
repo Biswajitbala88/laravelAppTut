651eded08b17b9debec2c00719f7a857e89aa560
Biswajitbala88 <biswajitbala88@gmail.com>
2025-10-28 11:36:53 +0530

    initial commit

---
+++ app/Http/Controllers/api/RegisterController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Illuminate\Http\JsonResponse;
+
+class RegisterController extends Controller
+{
+    //
+}
